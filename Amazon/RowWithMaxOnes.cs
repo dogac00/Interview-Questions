77ae62a089b6b88ba7e0925b76dc5114ca7e7644
Dogac <ndakyildiz@gmail.com>
2019-08-31 04:26:55 +0300

    Row With Max Ones is Added.

---
+++ Amazon/RowWithMaxOnes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestFibonacciSubset
+{
+    class RowWithMaxOnes
+    {
+        public static int GetRawWithMaxOnes(int[,] arr, int firstDim, int secondDim)
+        {
+            int max = 0;
+            int maxIndex = 0;
+            
+            for (int i = 0; i < firstDim; i++)
+            {
+                int arrMax = 0;
+
+                for (int j = 0; j < secondDim; j++)
+                    arrMax += arr[i, j];
+
+                if (arrMax > max)
+                    maxIndex = i;
+            }
+
+            return maxIndex;
+        }
+    }
+}
