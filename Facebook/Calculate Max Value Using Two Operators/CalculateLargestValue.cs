d8386162ef3ced8f86daa2c0cfdb47d00dbc2d17
Dogac <ndakyildiz@gmail.com>
2019-08-31 03:36:31 +0300

    Calculate Maximum Value using Two Signs Question is Added.

---
+++ Facebook/Calculate Max Value Using Two Operators/CalculateLargestValue.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestFibonacciSubset
+{
+    class CalculateLargestValue
+    {
+        public static int GetLargest(string str)
+        {
+            int result = str[0] - '0';
+
+            for (int i = 0; i < str.Length - 1; i++)
+            {
+                result = GetMaxValue(result, str[i + 1]);
+            }
+
+            return result;
+        }
+
+        static int GetMaxValue(int result, int second)
+        {
+            int num2 = second - '0';
+
+            if (result == 0 || num2 == 0)
+                return result + num2;
+
+            if (result == 1 || num2 == 1)
+                return result + num2;
+
+            return result * num2;
+        }
+    }
+}
