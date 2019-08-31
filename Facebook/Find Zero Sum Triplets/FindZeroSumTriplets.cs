5ff0e3898c3fb7108ed26e393124959f40200324
Dogac <ndakyildiz@gmail.com>
2019-08-31 03:50:01 +0300

    Brute Force Solution to Find Zero Sum Triplets Question.

---
+++ Facebook/Find Zero Sum Triplets/FindZeroSumTriplets.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LargestFibonacciSubset
+{
+    class FindZeroSumTriplets
+    {
+        public static List<Tuple<int, int, int>> FindTriplets(int[] arr)
+        {
+            List<Tuple<int, int, int>> triplets = new List<Tuple<int, int, int>>();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    for (int k = j + 1; k < arr.Length; k++)
+                    {
+                        if (arr[i] + arr[j] + arr[k] == 0)
+                            triplets.Add(Tuple.Create(arr[i], arr[j], arr[k]));
+                    }
+                }
+            }
+
+            return triplets;
+        }
+    }
+}
