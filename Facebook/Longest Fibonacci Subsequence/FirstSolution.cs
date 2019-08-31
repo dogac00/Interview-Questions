dc9763a6b8a03d5692ff671f1c781b4248e4d3ae
Dogac <ndakyildiz@gmail.com>
2019-08-31 03:13:29 +0300

    LongestFibonacciSubset Question is Added.

---
+++ Facebook/Longest Fibonacci Subsequence/FirstSolution.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace LargestFibonacciSubset
+{
+    class FirstSolution
+    {
+        static readonly Dictionary<int, bool> IsFibonacciDictionary = new Dictionary<int, bool>();
+
+        public static List<int> GetLongestFibonacciSubset(int[] arr)
+        {
+            List<int> result = new List<int>();
+
+            foreach (int num in arr)
+            {
+                if (IsFibonacci(num))
+                    result.Add(num);
+            }
+
+            return result;
+        }
+
+        static bool IsFibonacci(int num)
+        {
+            bool result;
+
+            if (IsFibonacciDictionary.TryGetValue(num, out result))
+            {
+                return result;
+            }
+
+            return IsFibonacciDictionary[num] = IsFibonacciInternal(num);
+        }
+
+        static bool IsFibonacciInternal(int num)
+        {
+            int result = 0;
+            int f1 = 0, f2 = 1;
+
+            while (result < num)
+            {
+                result = f1 + f2;
+                f1 = f2;
+                f2 = result;
+            }
+
+            return result == num;
+        }
+    }
+}
---
+++ Facebook/Longest Fibonacci Subsequence/SecondSolution.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace LargestFibonacciSubset
+{
+    class SecondSolution
+    {
+        public static List<int> GetLongestFibonacciSubset(int[] arr)
+        {
+            List<int> fibs = GenerateFibonacci(arr);
+            List<int> results = new List<int>();
+
+            foreach (int num in arr)
+            {
+                if (fibs.Contains(num))
+                {
+                    results.Add(num);
+                }
+            }
+
+            return results;
+        }
+
+        static List<int> GenerateFibonacci(int[] arr)
+        {
+            int result = 0, first = 0, second = 1, max = arr.Max();
+            List<int> results = new List<int>();
+
+            while (result < max)
+            {
+                result = first + second;
+                first = second;
+                second = result;
+                results.Add(result);
+            }
+
+            return results;
+        }
+    }
+}
